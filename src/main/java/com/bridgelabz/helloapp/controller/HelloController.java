2c54a0f9f13e23c2ccaf704ec91009bfb6522196
shivamrajput18 <shivamsingh80094@gmail.com>
2025-05-20 15:25:15 +0530

    [shivam] modifiy dto

--- src/main/java/com/bridgelabz/helloapp/controller/HelloController.java
+++ src/main/java/com/bridgelabz/helloapp/controller/HelloController.java
@@ -1,17 +1,40 @@
 package com.bridgelabz.helloapp.controller;
 
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bridgelabz.helloapp.dto.UserDto;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/")
 public class HelloController {
 
-    @GetMapping("/hello")
+    //uc1
+    @RequestMapping(value = {"", "/", "/home"})
     public String sayHello() {
-        return "Hello from BridgeLabz";
+        return "hello from Bridgelabz";
     }
 
+    //uc2
+    @RequestMapping(value = {"/query"})
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name;
+    }
+
+    //uc3
+    @RequestMapping(value = {"/parm/{name}"}, method = RequestMethod.GET)
+    public String sayHellobypath(@PathVariable String name) {
+        return "this msg from url Hello " + name;
+    }
+
+    //uc4
+    @PostMapping("/post")
+    public String sayhelloDto(@RequestBody UserDto user) {
+        return "Hello " + user.getFirstname() + " " + user.getLastname();
+    }
+
+    //uc5
+    @PutMapping("/put/{firstname}")
+    public String helloPUT(@PathVariable String firstname,
+                           @RequestParam String lastname) {
+        return "Hello " + firstname + " " + lastname;
+    }
 }

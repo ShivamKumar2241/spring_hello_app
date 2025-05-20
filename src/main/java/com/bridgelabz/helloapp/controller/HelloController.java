0b76d34fe08c7bca8ba3bd1a10719a843a2dadfb
shivamrajput18 <shivamsingh80094@gmail.com>
2025-05-20 15:04:27 +0530

    [Shivam] Add:dto package

--- src/main/java/com/bridgelabz/helloapp/controller/HelloController.java
+++ src/main/java/com/bridgelabz/helloapp/controller/HelloController.java
@@ -1,9 +1,7 @@
 package com.bridgelabz.helloapp.controller;
 
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bridgelabz.helloapp.dto.UserDto;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -13,5 +11,21 @@ public class HelloController {
     public String sayHello() {
         return "Hello from BridgeLabz";
     }
+
+    @RequestMapping(value = {"/query"})
+    public String sayHello(@RequestParam(value = "name") String name){
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = {"/parm/{name}"})
+    public String sayHellobypath(@PathVariable String name){
+        return "this msg from url Hello " + name;
+    }
+
+    @PostMapping("/post")
+    public String sayhelloDto(@RequestBody UserDto user){
+        return "Hello " + user.getFirstname() + " " + user.getLastname();
+    }
+
 
 }
